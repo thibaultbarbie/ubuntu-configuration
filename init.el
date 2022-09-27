66027f8f8f7ec661bfd5bface715c30edfca26cd
Thibault <thibault.barbie@free.fr>
2022-09-27 20:36:28 +0900

    Updated emacs configuration

--- init.el
+++ init.el
@@ -1,122 +1,229 @@
+(setq inhibit-startup-screen t)
+
+(blink-cursor-mode -1)  ; Disable the cursor blinking
+(scroll-bar-mode -1)    ; Disable the scroll bar
+(tool-bar-mode -1)      ; Disable the tool bar
+(tooltip-mode -1)       ; Disable the tooltips
+(set-fringe-mode 10)    ; Make some margin
+(menu-bar-mode -1)      ; Disable the menu bar
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Change colors of delimiters to help visualization
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Put custom automatic settings in another file
+(setq custom-file (concat user-emacs-directory "/custom.el"))
+
+(use-package general)
+
+;; Terminator-like window management
+(general-define-key
+ "C-S-E" 'split-window-right
+ "C-S-O" 'split-window-below
+ "C-S-W" 'delete-window
+ "C-S-<right>" 'other-window)
+
+;; Automatically update file
+(global-auto-revert-mode 1)
+
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 140)
+
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height 140)
+
+;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height 140 :weight 'regular)
+
+;; The fonts have to be installed at least once!
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons
+  :if (display-graphic-p))
+
 (require 'package)
 
-(add-to-list 'package-archives
-             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
-
-(add-to-list 'package-archives
-             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
-
-(add-to-list 'load-path "~/.emacs.d/site-lisp/")
-
-
-(setq package-list
-    '(use-package elpy magit org-bullets org-ref rust-mode racer flycheck flycheck-rust company lsp-mode lsp-ui ))
-
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
 
-(when window-system
-  (blink-cursor-mode 0)                           ; Disable the cursor blinking
-  (scroll-bar-mode 0)                             ; Disable the scroll bar
-  (tool-bar-mode 0)                               ; Disable the tool bar
-  (tooltip-mode 0))                               ; Disable the tooltips
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-(add-hook 'minibuffer-exit-hook
-	  '(lambda ()
-	     (let ((buffer "*Completions*"))
-	       (and (get-buffer buffer)
-		    (kill-buffer buffer)))))
-(setq make-backup-files nil)
-(global-linum-mode t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (deeper-blue)))
- '(inhibit-startup-screen t)
- '(line-spacing nil)
- '(org-agenda-files
-   (quote
-    ("~/Documents/phd/thesis.org" "~/Documents/daily_reports.org")))
- '(package-selected-packages
-   (quote
-    (org-ref org-babel-eval-in-repl org-re-reveal-ref org-bullets elpygen elpy magit))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+(use-package doom-themes
+  :init (load-theme 'doom-dark+ t))
+
+(use-package ivy
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+
+;; Shows the different option
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/Documents/Projects")
+    (setq projectile-project-search-path '("~/Documents/Projects")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
 ;; Conventional selection/deletion 
 (setq org-support-shift-select t)
 
-;; Terminator-like window management
-(global-set-key (kbd "C-S-E") 'split-window-right)
-(global-set-key (kbd "C-S-O") 'split-window-below)
-(global-set-key (kbd "C-S-W") 'delete-window)
-(global-set-key (kbd "C-S-<right>") 'other-window)
+(defun efs/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+			  '(("^ *\\([-]\\) "
+			     (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
-(require 'org)
-(define-key org-mode-map (kbd "C-S-<right>") nil)
-(define-key org-mode-map (kbd "C-S-<right>") 'other-window)
-(define-key global-map (kbd "C-c a") 'org-agenda)
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+		  (org-level-2 . 1.1)
+		  (org-level-3 . 1.05)
+		  (org-level-4 . 1.0)
+		  (org-level-5 . 1.1)
+		  (org-level-6 . 1.1)
+		  (org-level-7 . 1.1)
+		  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
 
-(elpy-enable)
-(require 'org-bullets)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-(setq visible-bell 1)
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 
 (require 'ox-latex)
 (with-eval-after-load 'ox-latex
    (add-to-list 'org-latex-classes
-                '("thibault-thesis"
-                  "\\documentclass{report}"
-                  ("\\chapter{%s}" . "\\chapter*{%s}")
+                '("ieeeconf"
+                  "\\documentclass[letterpaper, 10 pt, conference]{ieeeconf}
+		  \\usepackage[linesnumbered,ruled,vlined]{algorithm2e}"
                   ("\\section{%s}" . "\\section*{%s}")
                   ("\\subsection{%s}" . "\\subsection*{%s}")
-                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
-
-(with-eval-after-load 'ox-latex (add-to-list 'org-latex-classes '("jrm" "\\documentclass{jaciiiarticle}")))
+                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+		  )
+		)
+   )
 
 (setq org-latex-pdf-process (list "latexmk -shell-escape -bibtex -f -pdf %f"))
-
-(require 'org-ref)
-(global-set-key (kbd "C-c ]") 'org-ref-insert-cite-with-completion)
-(setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5))
-
-(setq-default word-wrap t)
-(global-visual-line-mode t)
-
-(setq org-image-actual-width nil)
-
-(global-set-key (kbd "C-c m") 'magit-status)
-
 (setq org-latex-caption-above nil)
 
-(setq magit-branch-read-upstream-first 'fallback)
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :hook (lsp-mode . efs/lsp-mode-setup)
+  :init
+  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
+  :config
+  (lsp-enable-which-key-integration t))
 
-(use-package flycheck
-  :hook (prog-mode . flycheck-mode))
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom))
+(use-package lsp-treemacs
+  :after lsp)
+(use-package lsp-ivy)
 
 (use-package company
-  :hook (prog-mode . company-mode)
-  :config (setq company-tooltip-align-annotations t)
-          (setq company-minimum-prefix-length 1))
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+         ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+         ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
 
-(use-package lsp-mode
-  :commands lsp
-  :config (require 'lsp-clients))
+(use-package company-box
+  :hook (company-mode . company-box-mode))
 
-(use-package lsp-ui)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
 
 (use-package rust-mode
   :hook (rust-mode . lsp))
@@ -128,14 +235,5 @@
 (use-package flycheck-rust
   :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
-(setq rust-rustfmt-bin "~/.cargo/bin/rustfmt")
-
-;; auto revert mode
-(global-auto-revert-mode 1)
-
-(setq racer-cmd "~/.cargo/bin/racer") ;; Rustup binaries PATH
-(setq racer-rust-src-path "~/rust/src") ;; Rust source code PATH
-
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
-(add-hook 'racer-mode-hook #'company-mode)
+;;(add-to-list 'load-path "~/Documents/Projects/bqn/bqn-mode")
+;;(require 'bqn-mode)
